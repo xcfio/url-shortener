c3b06db1a4d0afd05d156fd78904fdb8b9829f2a
xcfio <119097812+xcfio@users.noreply.github.com>
2025-04-04 16:41:09 +0600

    refactor blacklist logic and update domain fetching sources

--- src/utils/blacklist.ts
+++ src/utils/blacklist.ts
@@ -1,16 +1,30 @@
 let blacklist: Set<string> = new Set()
 // prettier-ignore
 const tld = RegExp(`(${[".cf", ".ga", ".gq", ".ml", ".nl", ".nz", ".tk", ".icu", ".online", ".click"].map((tld) => `\\${tld}`).join("|")})$`, "i")
-const website = ["url-xcf.netlify.app", "opensea.io", "pornhub.com", "sellix.io", "t.me", "telegram.com", "telegram.me"]
-
-fetch("https://raw.githubusercontent.com/nikolaischunk/discord-phishing-links/main/domain-list.json")
-    .then((buffer) => buffer.json())
-    .then((data) => (blacklist = new Set<string>(data.domains as Array<string>)))
-    .catch(console.trace)
 
 export function isBlacklisted(url: string) {
     if (tld.test(url)) return true
-    if (website.includes(url)) return true
     if (blacklist.has(url)) return true
     return false
 }
+
+fetch("https://big.oisd.nl/domainswild2")
+    .then((buffer) => buffer.text())
+    .then((x) => {
+        const list = x.split("\n")
+        for (const element of list) {
+            if (!element || element.startsWith("#")) continue
+            blacklist.add(element.split("/")[0])
+        }
+    })
+
+fetch("https://nsfw.oisd.nl/domainswild2")
+    .then((buffer) => buffer.text())
+    .then((x) => {
+        const list = x.split("\n")
+        for (const element of list) {
+            if (!element || element.startsWith("#")) continue
+            blacklist.add(element.split("/")[0])
+        }
+        console.log(blacklist)
+    })
